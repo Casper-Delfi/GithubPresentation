fbf9a9fc3b8e04a69dd50b4cf25016f37ff6e8e0
CAS-DELFI <111878243+Casper-Delfi@users.noreply.github.com>
2024-04-02 15:22:46 +0200

    Update main.py

--- hello/main.py
+++ hello/main.py
@@ -1,4 +1,4 @@
 from configparser import ConfigParser
 
 import pyodbc 
-cnxn = pyodbc.connect('DRIVER={Devart ODBC Driver for SQL Server};Server=myserver;Database=mydatabase;Port=myport;User ID=myuserid;Password=mypassword')
+cnxn = pyodbc.connect('DRIVER={Devart ODBC Driver for SQL Server};Server=myserver;Database=mydatabase;Port=4321;User ID=myuserid;Password=mypassword')
