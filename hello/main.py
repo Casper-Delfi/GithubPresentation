30f2fef0397f1539cb6da9a09e53dc1089730804
CAS-DELFI <111878243+Casper-Delfi@users.noreply.github.com>
2024-04-02 15:36:20 +0200

    Update main.py

--- hello/main.py
+++ hello/main.py
@@ -2,3 +2,6 @@ from configparser import ConfigParser
 
 import pyodbc 
 cnxn = pyodbc.connect('DRIVER={Devart ODBC Driver for SQL Server};Server=myserver;Database=mydatabase;Port=4321;User ID=myuserid;Password=mypassword')
+
+mongodb+srv://myDatabaseUser:D1fficultP%40ssw0rd@server.example.com/
+
